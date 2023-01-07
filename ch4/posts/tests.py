b400edb091070bf3ea8529aefe83cf4d641dcbd3
glewis <glelewis90@gmail.com>
2023-01-07 22:34:35 +0000

    Added more tests for homepage

--- ch4/posts/tests.py
+++ ch4/posts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Post
+from django.urls import reverse
 
 class PostTests(TestCase):
     @classmethod
@@ -8,3 +9,13 @@ class PostTests(TestCase):
     
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
+    
+    def test_url_exits_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_homepage(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
